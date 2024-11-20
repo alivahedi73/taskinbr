4c47d86be6ca78d317a1b78dd5b81bd8b8e97a11
Ali.vahedi <ali.bme73@gmail.com>
2024-11-20 15:55:30 +0330

    section2

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,40 @@
+import { useState } from "react";
 import "./Navbar.scss";
+import { data_image, data_menu } from "./data_navbar";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 const Navbar = () => {
-  return <>
-  <div className="navbar">
-    
-  </div>
-  </>;
+  let [hover, setHover] = useState(false);
+  let [click, setClick] = useState(0);
+  return (
+    <>
+      <div className="nav">
+        <div className={`nav__main ${hover ? "nav__active" : null}`}>
+          <div className="nav__main__sec1">
+            <img className="nav__main__sec1__img" src={data_image.src} alt="" />
+          </div>
+          <div className="nav__main__sec2">
+            {data_menu.map((item, index) => {
+              return (
+                <div
+                  className={`nav__main__sec2__item ${
+                    hover ? "nav__main__sec2__itemactive" : null
+                  }`}
+                  key={index}
+                  onMouseEnter={() => setHover(true)}
+                  onMouseLeave={() => setHover(!hover)}
+                >
+                  {item.name}
+                </div>
+              );
+            })}
+            <div className="nav__main__sec2__divsvg">
+              <SearchOutlinedIcon className="nav__main__sec2__divsvg__svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
---
+++ src/components/navbar/data_navbar.js
@@ -0,0 +1,25 @@
+export const data_image = {
+  src: "../../../public/assets/Image/homepage/logo/logo.png",
+};
+export const data_menu = [
+  {
+    id: 1,
+    name: "latest news",
+  },
+  {
+    id: 2,
+    name: "our company",
+  },
+  {
+    id: 3,
+    name: "careers",
+  },
+  {
+    id: 4,
+    name: "investor",
+  },
+  {
+    id: 5,
+    name: "media center",
+  },
+];
